942c1f5ab6d83b1200b3ddaa7b30a81723465b3a
Dator <Dator@DATOR>
2025-09-01 20:25:47 +0200

    Q02

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -13,6 +13,20 @@
 
             Console.WriteLine($"First out of stock product: {firstOutOfStockProduct?.ProductName}");
             #endregion
+            #region Q02
+            //2. Return the first product whose Price > 1000, unless there is no match, in which case null is returned.
+            var expensiveProduct = ProductList
+    .FirstOrDefault(p => p.UnitPrice > 1000);
+
+            if (expensiveProduct != null)
+            {
+                Console.WriteLine($"First product > 1000: {expensiveProduct.ProductName} - ${expensiveProduct.UnitPrice}");
+            }
+            else
+            {
+                Console.WriteLine("No product found with price > 1000");
+            }
+            #endregion
         }
     }
 }
